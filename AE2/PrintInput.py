a9e8c02acbdbd29f491476c0f4b846f9ebe21870
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-04-12 11:19:41 -0300

    ae2

---
+++ AE2/PrintInput.py
@@ -0,0 +1,10 @@
+numero = 5
+numero = numero + 5
+print("El resultado es", numero) # permite enviar mensaje a la consola
+
+print("Ingrese su nombre:")
+nombre = input() # permite capturar datos desde el teclado
+print("Su nombre es:", nombre)
+
+apellido = input("Ingrese su apellido: ")
+print("Su nombre completo es:", nombre, apellido)
