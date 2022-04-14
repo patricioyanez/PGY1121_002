a1c36a6abedd6e7fb69f67573d38c37234c6b247
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-04-14 11:04:27 -0300

    Update PrintInput.py

--- AE2/PrintInput.py
+++ AE2/PrintInput.py
@@ -8,3 +8,11 @@ print("Su nombre es:", nombre)
 
 apellido = input("Ingrese su apellido: ")
 print("Su nombre completo es:", nombre, apellido)
+
+anio = input("Ingrese su año de nacimiento: ")
+edad = 2022 - int(anio)  # funcion int() convierte un texto con nro en un int
+print("Ud tiene: ", edad, "años")
+
+# concatenar str con int
+mensaje = "Ud tiene " + str(edad) + " años" # str convierte el valor a un texto
+print(mensaje)
