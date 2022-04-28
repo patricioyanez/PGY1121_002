395ecffc236fcf19c28dd42a650b01e3cad922a7
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-04-28 11:37:42 -0400

    Create EjercicioContadorAcumulador.py

---
+++ AE2/EjercicioContadorAcumulador.py
@@ -0,0 +1,33 @@
+# 1.- definir variables
+acumulador = 0
+contador = 0
+
+# 2.- Solicitar la información al usuario
+print("****** Ingrese 3 numeros ******")
+n1 = input("Ingrese número 1:")
+n2 = input("Ingrese número 2:")
+n3 = input("Ingrese número 3:")
+
+# 3.- Procesar la info obtenida
+n1 = int(n1)
+n2 = int(n2)
+n3 = int(n3)
+
+if n1 > 0 and n1 % 2 == 0:
+    acumulador += n1 # acumulador = acumulador + n1
+else:
+    contador += 1 # contador = contador + 1
+
+if n2 > 0 and n2 % 2 == 0:
+    acumulador += n2 
+else:
+    contador += 1 
+    
+if n3 > 0 and n3 % 2 == 0:
+    acumulador += n3 
+else:
+    contador += 1 
+
+# 4.- Mostrar los resultados
+print("La suma de los numero es     :", acumulador)
+print("La cuenta de los numeros es  :", contador)
