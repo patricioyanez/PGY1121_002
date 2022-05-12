55ff2ca1cb2cc9329c78c2605e4ef4ab987c1760
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-05-12 12:10:57 -0400

    for

---
+++ EA3/EjemploFor1.py
@@ -0,0 +1,3 @@
+lista = range(101)
+for numero in lista:
+    print("Nro:", numero)
---
+++ EA3/EjemploFor2.py
@@ -0,0 +1,8 @@
+lista = range(1,10,2)
+for numero in lista:
+    print("Nro:", numero)
+
+
+lista = range(0,11,3)
+for numero in lista:
+    print("Nro:", numero)
